7d99ffb670c5e070ed23cf2a6fe284921a216615
ChastovSlava <crychs142@gmail.com>
2022-10-05 00:50:10 +0300

    Фикс ошибки при делении на 0

--- TraningSolution/TraningProgect_DivOperation/div_operation.c
+++ TraningSolution/TraningProgect_DivOperation/div_operation.c
@@ -1,12 +1,21 @@
 #include<stdio.h>
+#include<locale.h>
 int main() {
+	setlocale(LC_ALL, "rus");
 	int a, b;
 	float div;
-
+	printf("Введите 2 числа: ");
 	scanf_s("%d %d", &a, &b);
 	div = a / b;
-	printf("%d", div);
-
+	if (a == 0) {
+		div = abs(a / b);
+	}
+	else {
+		if (b == 0) {
+			printf("На ноль делить нельзя\n");
+		}
+	}
+	printf("Целая часть от деления этих чисел: %.0f\n", div);
 	system("pause");
 	return 0;
 }
